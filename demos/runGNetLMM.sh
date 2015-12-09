2d6fb75aaf49096b4504a2e97f0185c01ace61b0
brakitsch <barbara.rakitsch@googlemail.com>
2015-12-09 21:24:31 +0100

    added fixed effects file

--- demos/runGNetLMM.sh
+++ demos/runGNetLMM.sh
@@ -1,10 +1,13 @@
 #!/bin/bash 
 
 BFILE=./../data/1000G_chr22/chrom22_subsample20_maf0.10 #specify here bed basename
+FFILE=./../data/1000G_chr22/ones.txt
 PFILE=./out/pheno
 CFILE=./out/chrom22
-VFILE=./out/vstructures_thresh1e-6_wnd2000
+
 ASSOC0FILE=./out/lmm
+
+VFILE=./out/vstructures_thresh1e-6_wnd2000
 ASSOCFILE=./out/gnetlmm_thresh1e-6_wnd2000
 GFILE=./out/genes
 CIS_THRESH=1e-6
@@ -20,13 +23,13 @@ PLOTFILE=./out/power.pdf
 #./../GNetLMM/bin/gNetLMM_preprocess --compute_covariance --bfile $BFILE --cfile $CFILE
 
 # Run initial association scan
-for i in $(seq 0 10000 40000)
-do
-    ./../GNetLMM/bin/gNetLMM_analyse --initial_scan --bfile $BFILE --pfile $PFILE --cfile $CFILE.cov --assoc0file $ASSOC0FILE.startSnp_$i --startSnpIdx $i --nSnps 10000 
-done
+#for i in $(seq 0 10000 40000)
+#do
+#    ./../GNetLMM/bin/gNetLMM_analyse --initial_scan --bfile $BFILE --pfile $PFILE --cfile $CFILE.cov --assoc0file $ASSOC0FILE.startSnp_$i -#-startSnpIdx $i --nSnps 10000 --ffile $FFILE
+#done
 
 # Merging results
-#./../GNetLMM/bin/gNetLMM_preprocess --merge_assoc0_scan  --assoc0file $ASSOC0FILE 
+./../GNetLMM/bin/gNetLMM_analyse --merge_assoc0_scan  --assoc0file $ASSOC0FILE 
 
 # Compute marginal gene-gene correlations
 #./../GNetLMM/bin/gNetLMM_preprocess --gene_corr --pfile $PFILE --gfile $GFILE --bfile $BFILE
